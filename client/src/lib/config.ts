9f3eb826c9f884d06d2ee3d29307243991a1439b
Stains Rubus <stainsrubus@gmail.com>
2025-07-04 12:45:41 +0530

    config changed

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:4000/api/user';
-export const imgUrl = 'http://localhost:4000/api/user/file/local?key=';
-// export const baseUrl = 'https://santhi-crackers.onrender.com/api/user';
-// export const imgUrl = 'https://santhi-crackers.onrender.com/api/user/file/local?key=';
+// export const baseUrl = 'http://localhost:4000/api/user';
+// export const imgUrl = 'http://localhost:4000/api/user/file/local?key=';
+export const baseUrl = 'https://santhi-crackers.onrender.com/api/user';
+export const imgUrl = 'https://santhi-crackers.onrender.com/api/user/file/local?key=';
